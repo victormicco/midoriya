ef62ec0996a8736bd92fb48f3c21ccc7ce974b45
Victor Micco <victor.oficial093@gmail.com>
2024-04-01 21:27:10 -0300

    feat: footer

--- src/app/(application)/page.tsx
+++ src/app/(application)/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "../../components/navbar";
 import HeroSection from "../../components/heroSection";
+import Footer from "../../components/footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { Github, Home, Linkedin } from "lucide-react";
+import { ModeToggle } from "./mode-toggle";
+
+export default function Footer() {
+  return (
+    <footer className="fixed inset-x-0 bottom-0 border-t p-4 backdrop-blur-lg dark:border-gray-800">
+      <div className="container flex items-center justify-between  text-sm">
+        <div className="flex items-center gap-4">
+          <Home
+            className="h-5 w-5 cursor-pointer"
+            onClick={() => {
+              window.open("#");
+            }}
+            target="_blank"
+          />
+          <Linkedin
+            className="h-5 w-5 cursor-pointer"
+            onClick={() => {
+              window.open("https://www.linkedin.com/in/victormicco");
+            }}
+          />
+          <Github
+            className="h-5 w-5 cursor-pointer"
+            onClick={() => {
+              window.open("https://www.github.com/victormicco");
+            }}
+          />
+          <ModeToggle />
+        </div>
+        <p className="text-gray-500 dark:text-gray-400">
+          Developed with 💚 by Victor Micco
+        </p>
+      </div>
+    </footer>
+  );
+}
