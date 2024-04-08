e461403422272997b6608ad50c988cab8feb36ee
Victor Micco <victor.oficial093@gmail.com>
2024-04-07 22:13:58 -0300

    refactor: space added

--- src/components/AboutAccordion.tsx
+++ src/components/AboutAccordion.tsx
@@ -19,6 +19,7 @@ export function AboutAccordion() {
               window.open("https://github.com/victormicco/midoriya")
             }
           >
+            
             Github Repository
           </span>
         </AccordionContent>
